61e5fa16da349178db3335b2ae7c94335c59192d
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-20 18:21:08 +0200

    Removed commented out code

--- src/index.js
+++ src/index.js
@@ -2,47 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Movie from './Movie';
 
-/*
-class HelloWorld extends React.Component {
-
-    state = {
-        counter: 0
-    };
-
-    onCounterChange = (isDecrement) => {
-
-        let { counter } = this.state;
-        if(isDecrement) {
-            counter--;
-        } else {
-            counter++;
-        }
-
-        this.setState({counter});
-
-    }
-
-    render() {
-
-        const { user, age } = this.props;
-
-        return (
-            //className instead of class, htmlFor instead of for.
-            <div>
-                <h1>Hello my name is { user }. And I'am { age } years old!</h1>
-
-                <h4>{this.state.counter}</h4>
-                <button onClick={this.onCounterChange.bind(this, false)}>Increment</button>
-                <button onClick={this.onCounterChange.bind(this, true)}>Decrement</button>
-            </div>
-        )
-    }
-
-}
-
-ReactDOM.render(<HelloWorld user="John Snow" age={25}/>, document.getElementById('root'));
-*/
-
 class MovieSearch extends React.Component {
 
     state = {
